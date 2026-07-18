863ff1698e29ee44f0b26e1689cbdfc6efb80161
Griffin Lochner <griffinlochner@gmail.com>
2026-07-17 22:05:59 -0500

    add depth experiment

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import DepthPanoramaExperiment from './experiments/depth-panorama/DepthPanoramaExperiment'
 import LandingPage from './pages/LandingPage'
 import PlayerPage from './pages/PlayerPage'
 
 function App() {
   const pathname = window.location.pathname
+
+  if (pathname === '/experiments/depth-panorama') {
+    return <DepthPanoramaExperiment />
+  }
 
   if (pathname === '/player') {
     return <PlayerPage />
