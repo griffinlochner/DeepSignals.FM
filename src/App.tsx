b240877653a9f3cd208cea5d04d7893ca12e4a23
Griffin Lochner <griffinlochner@gmail.com>
2026-07-23 10:37:02 -0500

    add static player preview entry

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,20 @@ import EnvironmentLabPage from "./experiments/environment-lab/EnvironmentLabPage
 import LandingPage from "./pages/LandingPage";
 import PlayerPage from "./pages/PlayerPage";
 
+function normalizePathname(pathname: string) {
+  const withoutIndexHtml = pathname.endsWith("/index.html")
+    ? pathname.slice(0, -"/index.html".length)
+    : pathname;
+
+  if (withoutIndexHtml.length > 1 && withoutIndexHtml.endsWith("/")) {
+    return withoutIndexHtml.slice(0, -1);
+  }
+
+  return withoutIndexHtml || "/";
+}
+
 function App() {
-  const pathname = window.location.pathname;
+  const pathname = normalizePathname(window.location.pathname);
 
   if (pathname === "/experiments/environment-lab") {
     return <EnvironmentLabPage />;
